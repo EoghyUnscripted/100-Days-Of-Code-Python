5f95a1ae0345a000636f3331a9543b92fd09e6d7
Eoghan <eoghan@hulbert.info>
2021-08-13 19:43:02 -0700

    Added Day 2 Files

---
+++ Level 1 - Beginner/Day 2 - Tip Calculator/Exercises/exercise2-1.py
@@ -0,0 +1,47 @@
+"""
+
+INSTITUTION: UDEMY.COM
+
+COURSE: 100 Days of Code - The Complete Python Pro Bootcamp for 2021
+
+INSTRUCTOR: Dr. Angela Yu
+
+DESCRIPTION: 
+Master Python by building 100 projects in 100 days. 
+Learn to build websites, games, apps, plus scraping and data science
+
+DAY: 2
+EXERCISE: 2-1 Data Types
+
+INSTRUCTIONS:
+
+    Write a program that adds the digits in a 2 digit number. 
+    e.g. if the input was 35, then the output should be 3 + 5 = 8
+
+    Use the code provided -- do not change the existing code!
+
+    CODE:
+
+        two_digit_number = input("Type a two digit number: ")
+
+        # WRITE YOUR CODE HERE
+
+"""
+
+two_digit_number = input("Type a two digit number: ")
+
+if len(two_digit_number) > 2:
+  print("That is not a two digit number.\nTry, again.")
+  exit
+
+if len(two_digit_number) == 2:
+  
+  i = 0
+  num = 0
+
+  while i < 2:
+
+    num = num + int(two_digit_number[i])
+    i += 1
+      
+  print(num)
---
+++ Level 1 - Beginner/Day 2 - Tip Calculator/Exercises/exercise2-2.py
@@ -0,0 +1,40 @@
+"""
+
+INSTITUTION: UDEMY.COM
+
+COURSE: 100 Days of Code - The Complete Python Pro Bootcamp for 2021
+
+INSTRUCTOR: Dr. Angela Yu
+
+DESCRIPTION: 
+Master Python by building 100 projects in 100 days. 
+Learn to build websites, games, apps, plus scraping and data science
+
+DAY: 2
+EXERCISE: 2-2 BMI Calculator
+
+INSTRUCTIONS:
+
+    Write a program that calculates the Body Mass Index (BMI) from a user's weight and height.
+
+    The BMI is a measure of some's weight taking into account their height.
+    e.g. If a tall person and a short person both weigh the same amount, the short person is usually more overweight.
+
+    BMI = weight/height^2 = kg/meter^2
+
+    Use the code provided -- do not change the existing code!
+
+    CODE:
+
+        height = input("enter your height in m: ")
+        weight = input("enter your weight in kg: ")
+
+        # WRITE YOUR CODE HERE
+
+"""
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+bmi = float(weight) / float(height)**2
+print(int(bmi))
