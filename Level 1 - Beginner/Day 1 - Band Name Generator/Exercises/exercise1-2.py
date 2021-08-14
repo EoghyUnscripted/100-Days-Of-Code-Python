0a5ee7914592077cfbeb6503d0ff1a5779fafa7c
Eoghan <eoghan@hulbert.info>
2021-08-13 18:54:36 -0700

    Update file

--- Level 1 - Beginner/Day 1 - Band Name Generator/Exercises/exercise1-2.py
+++ Level 1 - Beginner/Day 1 - Band Name Generator/Exercises/exercise1-2.py
@@ -17,19 +17,20 @@ Learn to build websites, games, apps, plus scraping and data science
 
 ## Instructions:
 
-Write a program that prints the following:
+    Debug the following code:
 
-    Day 1 - Python Print Function
-    The function is declared like this:
-    print('what to print')
+    print(Day 1 - String Manipulation")
+    print("String Concatenation is done with the "+" sign.")
+        print('e.g. print("Hello " + "world")')
+    print(("New lines can be created with a backslash and n.")
 
-e.g. When you hit **run**, this is what should happen:
-![](https://cdn.fs.teachablecdn.com/q89uzhvRTf6CZHLtxLm6)
+e.g. When you hit **run**, there should be no errors and this is what should happen:
+
+ ![](https://cdn.fs.teachablecdn.com/BVP20Z2T1Gb4Pi6rOQah)
 
 """
 
-
-print(Day 1 - String Manipulation")
-print("String Concatenation is done with the "+" sign.")
-  print('e.g. print("Hello " + "world")')
-print(("New lines can be created with a backslash and n.")
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the " + '"+"' + " sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
