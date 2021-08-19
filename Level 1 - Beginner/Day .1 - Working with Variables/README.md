2614f268334133156c335774d6a18b275ced259a
Eoghan <eoghan@hulbert.info>
2021-08-18 18:02:00 -0700

    Update Comments

---
+++ Level 1 - Beginner/Day .1 - Working with Variables/README.md
@@ -0,0 +1,16 @@
+# Day 1 Working with Variables
+
+## Project: Band Name Generator
+
+### Instructions
+
+1. Create a greeting for your program.
+
+2. Ask the user for the city that they grew up in.
+
+3. Ask the user for the name of a pet.
+
+4. Combine the name of their city and pet and show them their band name.
+
+5. Make sure the input cursor shows on a new line, see the example at:
+   [Band Name Generator Demo](https://band-name-generator-end.appbrewery.repl.run/)
--- Level 1 - Beginner/Day .1 - Working with Variables/band_name_generator.py
+++ Level 1 - Beginner/Day .1 - Working with Variables/band_name_generator.py
@@ -1,9 +1,7 @@
 """
 
 INSTITUTION: UDEMY.COM
-
 COURSE: 100 Days of Code - The Complete Python Pro Bootcamp for 2021
-
 INSTRUCTOR: Dr. Angela Yu
 
 DESCRIPTION: 
@@ -13,15 +11,6 @@ Learn to build websites, games, apps, plus scraping and data science
 DAY: 1
 PROJECT: BAND NAME GENERATOR
 LEVEL: BEGINNER
-
-INSTRUCTIONS:
-
-    1. Create a greeting for your program.
-    2. Ask the user for the city that they grew up in.
-    3. Ask the user for the name of a pet.
-    4. Combine the name of their city and pet and show them their band name.
-    5. Make sure the input cursor shows on a new line, see the example at:
-       https://band-name-generator-end.appbrewery.repl.run/
 
 """
 
