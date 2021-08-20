433761d532827d488ce92166916337384533cff1
Eoghan <eoghan@hulbert.info>
2021-08-19 21:35:13 -0700

    Update Files and Comments

--- Level 1 - Beginner/Day .3 - Conditionals and Logical Operators/Exercises/exercise3-4.py
+++ Level 1 - Beginner/Day .3 - Conditionals and Logical Operators/Exercises/exercise3-4.py
@@ -14,7 +14,42 @@ LEVEL: Beginner
 
 """
 
-print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+print("Welcome to Python Pizza Deliveries!")    # Prints welcome greeting
+size = input("What size pizza do you want? S, M, or L ")    # Gets size choice from user
+add_pepperoni = input("Do you want pepperoni? Y or N ") # Gets peperroni choice from user
+extra_cheese = input("Do you want extra cheese? Y or N ")   # Gets extra cheese choice from user
+
+# Convert inputs to uppercase
+size = size.upper()
+add_pepperoni = add_pepperoni.upper()
+extra_cheese = extra_cheese.upper()
+
+# Check that entries are valid
+if size not in ['L','M','S']:   # Checks that size is S, M or L for input
+    print('Please enter S, M, or L for size.')  # Outputs message if error
+elif add_pepperoni not in ['N', 'Y']:   # Checks that input is Y or N
+    print('Please use Y or N to make your selection for pepperoni.')    # Outputs message if error
+elif extra_cheese not in ['N', 'Y']:    # Checks that input is Y or N
+    print('Please use Y or N to make your selection for extra cheese.') # Outputs message if error
+
+# Calculate order price
+if size == 'L': # Calculate large pizza
+    order = 25  # Sets base price
+    if add_pepperoni == 'Y':   # Adds price for pepperoni
+        order += 3  # Calculates added price
+    if extra_cheese == 'Y':    # Adds price for extra cheese
+        order += 1  # Calculates added price
+elif size == 'M':   # Calculate medium pizza
+    order = 20  # Sets base price
+    if add_pepperoni == 'Y':   # Adds price for pepperoni
+        order += 3  # Calculates added price
+    if extra_cheese == 'Y':    # Adds price for extra cheese
+        order += 1  # Calculates added price
+elif size == 'S':   # Calculate small pizza
+    order = 15  # Sets base price
+    if add_pepperoni == 'Y':   # Adds price for pepperoni
+        order += 2  # Calculates added price
+    if extra_cheese == 'Y':    # Adds price for extra cheese
+        order += 1  # Calculates added price
+
+print(f'Your final bill is: ${order}.')  # Output total to user
--- Level 1 - Beginner/Day .3 - Conditionals and Logical Operators/Exercises/exercise3-5.py
+++ Level 1 - Beginner/Day .3 - Conditionals and Logical Operators/Exercises/exercise3-5.py
@@ -12,8 +12,49 @@ DAY: 3
 EXERCISE: 3-5 Love Calculator
 LEVEL: Beginner
 
+T occurs 0 times
+R occurs 1 time
+U occurs 2 times
+E occurs 2 times
+Total = 5
+
+L occurs 1 time
+O occurs 0 times
+V occurs 0 times
+E occurs 2 times
+Total = 3
+
 """
 
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
+
+combined_names = (name1 + name2).lower()
+
+# Get count of each letter for T, R, U, E from the names
+t = combined_names.count('t')
+r = combined_names.count('r')
+u = combined_names.count('u')
+e = combined_names.count('e')
+
+true_count = t + r + u + e  # Total the count of all letters
+
+# Get count of each letter for L, O, V, E from the names
+l = combined_names.count('l')
+o = combined_names.count('o')
+v = combined_names.count('v')
+e = combined_names.count('e')
+
+love_count = l + o + v + e  # Total the count of all letters
+
+# Combine the counts from each word
+score = true_count + love_count
+
+# Get result statement to output to user
+if score < 10 or score > 90:
+    print(f'Your score is {score}, you go together like coke and mentos.')
+elif score >= 40 or score <= 50:
+    print(f'Your score is {score}, you are alright together.')
+else:
+    print(f'Your score is {score}.')
