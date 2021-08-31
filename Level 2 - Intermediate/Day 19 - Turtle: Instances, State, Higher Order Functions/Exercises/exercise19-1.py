6dc6bf25b74af338740fc30caedf5e7af51ad71b
Eoghan <eoghan@hulbert.info>
2021-08-31 14:04:32 -0700

    Updated Comments

--- Level 2 - Intermediate/Day 19 - Turtle: Instances, State, Higher Order Functions/Exercises/exercise19-1.py
+++ Level 2 - Intermediate/Day 19 - Turtle: Instances, State, Higher Order Functions/Exercises/exercise19-1.py
@@ -18,33 +18,41 @@ LEVEL: Intermediate
 
 import turtle as t
 
-timmy = t.Turtle()
+timmy = t.Turtle()  # Create new turtle object
 
 
 def forward():
-    timmy.forward(10)
+    """Function used to move turtle object forward."""
+    timmy.forward(10)   # Move forward 10 spaces
 
 
 def backward():
-    timmy.back(10)
+    """Function used to move turtle object backward."""
+    timmy.back(10)  # Move backward 10 spaces
 
 
 def counter():
-    timmy.left(10)
+    """Function used to turn turtle object counter-clockwise."""
+    timmy.left(10)  # Turn left by 10 degrees
 
 
 def clockwise():
-    timmy.right(10)
+    """Function used to turn turtle object clockwise."""
+    timmy.right(10)     # Turn right by 10 degrees
 
 
 def clear_screen():
-    timmy.clear()
+    """Function used to clear screen and move turtle to original position."""
+    timmy.clear()   # Clear screen of drawing
+    timmy.penup()   # Pen up to prevent drawing
+    timmy.home()    # Move turtle object back to original position
+    timmy.pendown()     # Pen down to start drawing
 
 
-t.listen()
-t.onkey(fun=forward, key="w")
-t.onkey(fun=backward, key="s")
-t.onkey(fun=counter, key="a")
-t.onkey(fun=clockwise, key="d")
-t.onkey(fun=clear_screen, key="c")
+t.listen()      # Listens for key inputs
+t.onkey(fun=forward, key="w")   # When 'w' is pressed, move forward
+t.onkey(fun=backward, key="s")  # When 's' is pressed, move backward
+t.onkey(fun=counter, key="a")   # When 'a' is pressed, turn counter-clockwise
+t.onkey(fun=clockwise, key="d")     # When 'd' is pressed, turn clockwise
+t.onkey(fun=clear_screen, key="c")  # When 'c' is pressed, clear screen, return to original position
 t.exitonclick()    # Keeps screen open until clicked
