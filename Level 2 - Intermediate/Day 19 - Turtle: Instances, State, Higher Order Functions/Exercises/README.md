16c96af65017e8b9ee7c73c8abf517f55119b47d
Eoghan <eoghan@hulbert.info>
2021-08-31 13:56:13 -0700

    Updated Files

--- Level 2 - Intermediate/Day 19 - Turtle: Instances, State, Higher Order Functions/Exercises/README.md
+++ Level 2 - Intermediate/Day 19 - Turtle: Instances, State, Higher Order Functions/Exercises/README.md
@@ -1,19 +1,19 @@
-# Day X 
+# Day 19 Turtle: Instances, State, Higher Order Functions
 
-## Exercise X-Y: 
+## Exercise 19-1: Etch-A-Sketch
 
 ### Instructions
 
-
-
-### Example Input
-
-
+1. Using Turtle, create codes that will allow users to enter:
+   1. `w` to move forward
+   2. `s` to move backwards
+   3. `a` to move counter-clockwise
+   4. `d` to move clockwise
+   5. `c` to clear the screen
+2. Make sure to include an `onclick()` option to close screen
+3. You can leave the turtle object at default settings
 
 ### Example Output
 
-
-
-### Hint
-
+![Image 19-1](../Images/exercise19-1.png)
 
--- Level 2 - Intermediate/Day 19 - Turtle: Instances, State, Higher Order Functions/Exercises/exercise19-1.py
+++ Level 2 - Intermediate/Day 19 - Turtle: Instances, State, Higher Order Functions/Exercises/exercise19-1.py
@@ -6,16 +6,45 @@ COURSE: 100 Days of Code - The Complete Python Pro Bootcamp for 2021
 
 INSTRUCTOR: Dr. Angela Yu
 
-DESCRIPTION: 
-Master Python by building 100 projects in 100 days. 
+DESCRIPTION:
+Master Python by building 100 projects in 100 days.
 Learn to build websites, games, apps, plus scraping and data science
 
 DAY: 19
-EXERCISE: 19-1 Turtle: Etch-A-Sketch App
+EXERCISE: 19-1 Turtle: Etch-A-Sketch
 LEVEL: Intermediate
 
-INSTRUCTIONS:
-
-    
-
 """
+
+import turtle as t
+
+timmy = t.Turtle()
+
+
+def forward():
+    timmy.forward(10)
+
+
+def backward():
+    timmy.back(10)
+
+
+def counter():
+    timmy.left(10)
+
+
+def clockwise():
+    timmy.right(10)
+
+
+def clear_screen():
+    timmy.clear()
+
+
+t.listen()
+t.onkey(fun=forward, key="w")
+t.onkey(fun=backward, key="s")
+t.onkey(fun=counter, key="a")
+t.onkey(fun=clockwise, key="d")
+t.onkey(fun=clear_screen, key="c")
+t.exitonclick()    # Keeps screen open until clicked
