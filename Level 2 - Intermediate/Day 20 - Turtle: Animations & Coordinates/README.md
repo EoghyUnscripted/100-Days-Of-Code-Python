96111ca7ec159e52407ef278000f8e0b9c0cc267
Eoghan <eoghan@hulbert.info>
2021-09-05 20:03:58 -0700

    Add and Update Day 20 Files

---
+++ Level 2 - Intermediate/Day 20 - Turtle: Animations & Coordinates/README.md
@@ -0,0 +1,27 @@
+# Day 20 Turtle: Animations & Coordinates
+
+## Project: Snake Game Part 1
+
+Using the Turtle package, we will build a snake game. This will include the object shape, collision detection, user key listeners and a scoreboard.
+
+### Instructions
+
+1. Create a snake body
+2. Create a function to constantly move the snake forward
+3. Allow user key input to change direction of snake:
+   1. North (90 Degrees) = `Up`
+   2. South (270 Degrees) = `Down`
+   3. East (0 Degrees) = `Right`
+   4. West (180) = `Left`
+
+### Example Output
+
+![Snake Game Part 1](Images/snake_game_part1.png)
+
+#### Demo Issues
+
+May not work in Replit.
+
+### Replit Demo
+
+[Replit Demo - Snake Game](https://replit.com/@EoghyUnscripted/Snake-Game-)
---
+++ Level 2 - Intermediate/Day 20 - Turtle: Animations & Coordinates/snake_game_part1.py
@@ -0,0 +1,48 @@
+"""
+
+INSTITUTION: UDEMY.COM
+COURSE: 100 Days of Code - The Complete Python Pro Bootcamp for 2021
+INSTRUCTOR: Dr. Angela Yu
+
+DESCRIPTION:
+Master Python by building 100 projects in 100 days.
+Learn to build websites, games, apps, plus scraping and data science
+
+DAY: 20
+PROJECT: Snake Game
+LEVEL: Intermediate
+
+"""
+import time
+from turtle import Screen, Turtle
+from Modules import snake
+
+# Screen Setup
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+sammy = snake.Snake()
+screen.listen()     # Listens for key inputs
+
+
+def game_play():
+    """Function used to start game."""
+
+    game_is_running = True
+
+    while game_is_running:
+        screen.update()
+        time.sleep(0.3)
+        sammy.move()
+
+
+screen.onkey(sammy.left, "Left")   # When 'Left Arrow' is pressed, turn West
+screen.onkey(sammy.right, "Right")     # When 'Right Arrow' is pressed, turn East
+screen.onkey(sammy.up, "Up")   # When 'Up Arrow' is pressed, turn North
+screen.onkey(sammy.down, "Down")     # When 'Down Arrow' is pressed, turn South
+screen.onkey(game_play, "space")    # When 'Space' is pressed, start game
+
+screen.exitonclick()
