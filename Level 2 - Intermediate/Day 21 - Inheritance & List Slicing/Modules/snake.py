19d163dcf394d6a66af1ac7c21eed7534b42e8c4
Eoghan <eoghan@hulbert.info>
2021-09-05 20:04:05 -0700

    Add and Update Day 21 Files

---
+++ Level 2 - Intermediate/Day 21 - Inheritance & List Slicing/Modules/snake.py
@@ -0,0 +1,56 @@
+from turtle import Screen, Turtle
+
+position_list = [(0, 0), (-20, 0), (-40, 0)]
+distance = 20
+up = 90
+down = 270
+left = 180
+right = 0
+
+
+class Snake:
+    """Class used to create initial snake body and add functions for snake objects."""
+
+    def __init__(self):
+        self.turtle_list = []
+        self.create_snake()
+        self.head = self.turtle_list[0]
+
+    def up(self):
+        """Function used to turn turtle object counter-clockwise."""
+        if self.head.heading() != down:
+            self.head.setheading(90)  # Turn North
+
+    def down(self):
+        """Function used to turn turtle object clockwise."""
+        if self.head.heading() != up:
+            self.head.setheading(270)  # Turn South
+
+    def left(self):
+        """Function used to turn turtle object clockwise."""
+        if self.head.heading() != right:
+            self.head.setheading(180)  # Turn West
+
+    def right(self):
+        """Function used to turn turtle object clockwise."""
+        if self.head.heading() != left:
+            self.head.setheading(0)  # Turn East
+
+    def move(self):
+        """Function used to move snake object forward."""
+        for segment in range(len(self.turtle_list) - 1, 0, -1):
+            new_x = self.turtle_list[segment - 1].xcor()
+            new_y = self.turtle_list[segment - 1].ycor()
+            self.turtle_list[segment].goto(new_x, new_y)
+        self.turtle_list[0].forward(distance)
+
+    def create_snake(self):
+        """Function used to create initial snake objet for Turtle screen."""
+        # Fill the initial turtle list for snake body
+        for position in position_list:
+            new_turtle = Turtle()
+            new_turtle.color("grey")
+            new_turtle.shape("square")
+            new_turtle.penup()
+            new_turtle.goto(position)
+            self.turtle_list.append(new_turtle)
---
+++ Level 2 - Intermediate/Day 21 - Inheritance & List Slicing/README.md
@@ -0,0 +1,33 @@
+# Day 20 Turtle: Animations & Coordinates
+
+## Project 2: Turtle Racing
+
+Using the Turtle package, we will build a snake game. This will include the object shape, collision detection, user key listeners and a scoreboard.
+
+### Instructions
+
+1. Create a snake body
+2. Create a function to constantly move the snake forward
+3. Allow user key input to change direction of snake:
+   1. North (90 Degrees) = `Up`
+   2. South (270 Degrees) = `Down`
+   3. East (0 Degrees) = `Right`
+   4. West (180) = `Left`
+4. Create `snake food`
+5. Create a `scoreboard` object to track the current score
+6. Create detection codes for:
+   1. Collision with food
+   2. Collision with walls
+   3. Collision with tail
+
+### Example Output
+
+
+
+#### Demo Issues
+
+May not work in Replit.
+
+### Replit Demo
+
+[Replit Demo - Snake Game](https://replit.com/@EoghyUnscripted/Snake-Game)
