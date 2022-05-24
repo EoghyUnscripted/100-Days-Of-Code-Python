674c56fd6546e3e37b35027e68e0e24cf0121f97
Eoghan <71570133+EoghyUnscripted@users.noreply.github.com>
2022-05-24 02:25:46 -0700

    Create module files

---
+++ Level 3 - Advanced/Day 32 - SMTPLIB & Datetime/Modules/birthday_loader.py
@@ -0,0 +1,62 @@
+"""
+
+INSTITUTION: UDEMY.COM
+COURSE: 100 Days of Code - The Complete Python Pro Bootcamp for 2022
+INSTRUCTOR: Dr. Angela Yu
+
+DESCRIPTION:
+Master Python by building 100 projects in 100 days.
+Learn to build websites, games, apps, plus scraping and data science
+
+DAY: 32
+PROJECT: Automated Birthday Emailer
+LEVEL: Intermediate
+
+"""
+
+import pandas
+from datetime import datetime as dt
+from random import choice
+
+def letter_generator(person):
+    """ Function used to draft the body content of the email using an external text file. """
+    
+    # List of letter choices
+    files = [
+        "Level 3 - Advanced/Day 32 - SMTPLIB & Datetime/Input/letter_1.txt",
+        "Level 3 - Advanced/Day 32 - SMTPLIB & Datetime/Input/letter_2.txt",
+        "Level 3 - Advanced/Day 32 - SMTPLIB & Datetime/Input/letter_3.txt"
+    ]
+    
+    file_name = choice(files)   # Get a random file path for new birthday email
+    
+    # Open file using name parameter
+    with open(f"{file_name}") as body:
+        message = body.read()   # Read the content of the file
+        email_body = message.replace("[NAME]", f"{person}")     # Replace name placeholder with recipient name
+    
+    return email_body
+
+def check_birthdays(birthdays):
+    """ Function used to check the current day of the week and check for matching birthdays in external csv file. """
+    
+    today = (dt.now().month, dt.now().day)  # Creat a tuple with month and day
+    
+    if today in birthdays:  # If month, day is a key in birthdays dictionary
+        
+        return birthdays[today]
+    
+    else:   # If there is no match
+        
+        return False
+
+def load_file():
+    """ Function used to load birthday content from csv file. """
+    
+    # Get data from the provided csv file
+    data = pandas.read_csv("Level 3 - Advanced/Day 32 - SMTPLIB & Datetime/Modules/birthdays.csv")
+
+    # Convert to dictionary
+    birthday_dict = {(row.month, row.day): row for (index, row) in data.iterrows()}
+
+    return check_birthdays(birthday_dict)
---
+++ Level 3 - Advanced/Day 32 - SMTPLIB & Datetime/Modules/emailer.py
@@ -0,0 +1,51 @@
+"""
+
+INSTITUTION: UDEMY.COM
+COURSE: 100 Days of Code - The Complete Python Pro Bootcamp for 2022
+INSTRUCTOR: Dr. Angela Yu
+
+DESCRIPTION:
+Master Python by building 100 projects in 100 days.
+Learn to build websites, games, apps, plus scraping and data science
+
+DAY: 32
+PROJECT: Automated Birthday Emailer
+LEVEL: Intermediate
+
+"""
+
+import smtplib    
+
+def send_Birthday_Wishes(send_from, password, recipient, subject, message):
+    """ Function used to send a new email with a motivational message. """
+    
+    # NOTE: This setup is using Gmail SMTP. You will want to use the SMTP based on your own email host.
+    #       If using Gmail, check your security settings and set up an App Password for enhanced security.
+
+    # SMTP Connection Setup
+
+    try:    # Attempt to connect to email server
+        
+        connection = smtplib.SMTP_SSL("smtp.gmail.com", 465)    # Create a secure SSL connection
+        connection.ehlo()   # Identify your email server to the host
+        connection.login(user=send_from, password=password)     # Pass through your credentials
+        
+    except smtplib.SMTPAuthenticationError:     # If unable to authenticate
+        
+        print("Could not connect. Please check your authentication settings on your email server.")
+        
+    except TimeoutError:    # If connection timed out or server did not respond
+        
+        print("Unable to reach the host server. Please check your SSL and port settings.")
+        
+    else:   # Set up email content
+
+        # Structure the body content
+        message = f"Subject: {subject}\n\n{message}".format(subject, message)    # Structure the body content
+
+        connection.sendmail(from_addr=send_from, to_addrs=recipient, msg=message)   # Pass data to server for email message
+        
+    finally:    # Always execute for security
+        
+        connection.close()  # Close the connection
+        
