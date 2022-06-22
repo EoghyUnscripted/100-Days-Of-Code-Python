e7949298d4d2f4452425e1f66099a01e6049d3da
Eoghan <71570133+EoghyUnscripted@users.noreply.github.com>
2022-06-21 21:29:32 -0700

    Update main project file

--- Level 3 - Advanced/Day 54 - Intro to Flask/hello_flask.py
+++ Level 3 - Advanced/Day 54 - Intro to Flask/hello_flask.py
@@ -1,4 +1,19 @@
-from re import L
+"""
+
+INSTITUTION: UDEMY.COM
+COURSE: 100 Days of Code - The Complete Python Pro Bootcamp for 2022
+INSTRUCTOR: Dr. Angela Yu
+
+DESCRIPTION:
+Master Python by building 100 projects in 100 days.
+Learn to build websites, games, apps, plus scraping and data science
+
+DAY: 54
+PROJECT: Hello Flask
+LEVEL: Advanced
+
+"""
+
 from flask import Flask
 import time
 
