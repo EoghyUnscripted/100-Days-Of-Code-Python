b024e3a7814233d829ea61950fb7410a6fb07c53
Eoghan <71570133+EoghyUnscripted@users.noreply.github.com>
2022-06-29 20:26:56 -0700

    Create main project app file

---
+++ Level 3 - Advanced/Day 56 - Rendering HTML & Static Files/server.py
@@ -0,0 +1,35 @@
+"""
+
+INSTITUTION: UDEMY.COM
+COURSE: 100 Days of Code - The Complete Python Pro Bootcamp for 2022
+INSTRUCTOR: Dr. Angela Yu
+
+DESCRIPTION:
+Master Python by building 100 projects in 100 days.
+Learn to build websites, games, apps, plus scraping and data science
+
+DAY: 56
+PROJECT: Name Card
+LEVEL: Advanced
+
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+    
+# Main route
+@app.route("/")
+def hello():
+    
+    return render_template("index.html")
+
+# Profile route
+@app.route("/profile")
+def profile():
+    
+    return render_template("profile.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
