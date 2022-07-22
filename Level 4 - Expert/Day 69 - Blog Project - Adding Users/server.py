9961c6f9dd697ec7842f6b913ec11a774e4f9446
Eoghan <71570133+EoghyUnscripted@users.noreply.github.com>
2022-07-21 21:56:28 -0700

    Trim code

--- Level 4 - Expert/Day 69 - Blog Project - Adding Users/server.py
+++ Level 4 - Expert/Day 69 - Blog Project - Adding Users/server.py
@@ -59,13 +59,18 @@ def load_user(user_id):
 
 
 def admin_only(f):
+    
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        
         #If id is not 1 then return abort with 403 error
         if current_user.id != 1:
+            
             return abort(403)
+        
         #Otherwise continue with the route function
         return f(*args, **kwargs)        
+    
     return decorated_function
 
 
